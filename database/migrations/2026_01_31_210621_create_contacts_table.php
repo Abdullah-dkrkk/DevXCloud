5025b97d05880c7b0db535481623c2747f4499ee
Abdullah-dkrkk <abdullahsaifullah988@gmail.com>
2026-02-02 00:57:27 +0500

    Minor Modification

--- database/migrations/2026_01_31_210621_create_contacts_table.php
+++ database/migrations/2026_01_31_210621_create_contacts_table.php
@@ -1,26 +1,37 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration   // ✅ FIX HERE
+return new class extends Migrationa
 {
     public function up(): void
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+
             $table->string('first_name', 50);
             $table->string('last_name', 50);
             $table->string('job_title', 100)->nullable();
             $table->string('work_email');
             $table->string('company', 100);
+
             $table->enum('employed', ['yes', 'no']);
+
             $table->string('mobile', 20);
+
             $table->string('country', 100);
             $table->string('language', 50);
+
+            // compliance / audit
             $table->boolean('terms_accepted')->default(false);
             $table->timestamp('terms_accepted_at')->nullable();
+
+            // optional useful data
             $table->ipAddress('ip_address')->nullable();
             $table->string('user_agent')->nullable();
+
             $table->timestamps();
         });
     }
