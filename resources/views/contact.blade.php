888ce4a88719f96a87dd171726d51d270194c96a
Abdullah-dkrkk <abdullahsaifullah988@gmail.com>
2026-02-08 19:50:03 +0500

    Some further development Progress on about and contact Pages

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,79 +5,6 @@
 
 
 @section('content')
-    <style>
-        .contact-banner h1 {
-            font-size: clamp(32px, 3.5vw, 46px);
-            line-height: clamp(48px, 4.5vw, 60px);
-        }
-        .contact-banner {
-            background: linear-gradient(
-                180deg,
-                #0f1e46 0%,
-                #122a66 18%,
-                #143b85 54%,
-                #143b85 60%,
-                #122a66 94%,
-                #0f1e46 100%
-            )
-        }
-        .contact-banner .container .row .col-lg-12 {
-            min-height: 300px;
-        }
-
-        .contact-content {
-            padding-top: 92px;
-            padding-bottom: 92px;
-        }
-        @media only screen and (max-width: 1400px) {
-            .container {
-                min-width: 100%;
-            }
-        }
-        @media only screen and (max-width: 1330px) {
-            nav .theme__btn {
-                display:none;
-            }
-            .navbar-expand-xl .navbar-nav .nav-link {
-                text-wrap:nowrap;
-            }
-            body nav .nav-item span {
-                font-size: 12px;
-            }
-            nav.navbar-expand-xl .container {
-                padding-right: 0px;
-                padding-left: 0px;
-            }
-            .navbar-brand {
-                padding-left: 16px;
-            }
-            .navbar-toggler {
-                margin-right: 16px;
-            }
-        }
-        @media only screen and (max-width: 1330px) and (min-width: 1024px) {
-            .navbar-expand-xl .navbar-nav {
-                margin-right: 16px;
-            }
-        }
-        @media only screen and (max-width: 1201px) {
-            .navbar-expand-xl .navbar-nav li {
-                min-width: 100% !important;
-                padding-right: 16px !important;
-                padding-left: 16px !important;
-            }
-            .navbar-expand-xl .navbar-nav {
-                float:left;
-            }
-            .navbar-collapse{
-                margin-top: -10px;
-                background:white;   
-                padding-top: 12px;
-                padding-bottom: 24px;
-            }
-        }
-
-    </style>
     <!-- banner starts from here -->
     <section class="contact-banner">
         <div class="container">
@@ -95,9 +22,9 @@
     <section class="contact-section-wrapper">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6 d-flex align-items-center justify-content-center">
+                <div class="col-lg-5 d-flex align-items-center justify-content-center">
                     <div class="inner-wrapper d-flex align-items-start justify-content-center flex-column">
-                        <h2 class="mb-sm-0 mb-3">Let’s Start Your Growth Journey</h2>
+                        <h2 class="mb-2 devx__color-primary">Let’s Start Your Growth Journey</h2>
                         <p>Tell us a bit about your business. We’ll review your goals and get back with the next steps to build your custom growth system.</p>
                         <ul class="list-unstyled"> 
                             <li class="py-2">
@@ -115,9 +42,8 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-lg-6 d-flex align-items-center justify-content-center">
-                    <div class="inner-wrapper w-100 d-flex flex-column align-items-start justify-content-center">
-
+                <div class="col-lg-7 d-flex align-items-center justify-content-center">
+                    <div class="inner-wrapper w-100 d-flex flex-column align-items-center justify-content-center">
                         @if (session('success'))
                             <div class="alert alert-success w-100">
                                 {{ session('success') }}
@@ -125,6 +51,9 @@
                         @endif
 
                         <form method="POST" action="{{ route('contact.submit') }}" novalidate class="contact-form">
+
+                            <h2 class="devx__color-primary text-center mb-3">Contact Us</h2>
+                            
                             @csrf
 
                             <!-- Row 1: Full Name + Work Email -->
